7d5afd6d54f0f07537943e195d6b7e97162e07cd
DennyMphethi <dennymarumo44@gmail.com>
2025-06-21 13:04:16 +0200

    Create AppNavigator.js

---
+++ client/navigation/AppNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/Auth/LoginScreen';
+import RegisterScreen from '../screens/Auth/RegisterScreen';
+import DashboardScreen from '../screens/Customer/DashboardScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
